80e5936c328fee74d7bf581b64a85d2a78a03c1b
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-02 09:08:05 +0200

    initial project commit

---
+++ theme.js
@@ -0,0 +1,25 @@
+const colors = {
+  primary: {
+    pinkGradient: "linear-gradient(to bottom, #F02AA6, #FF6F48)",
+    purpleGradient: "linear-gradient(to bottom, #4851FF, #F02AA6)",
+    darkBlue: "#13183F",
+    lightBlue: "#666CA3",
+    grey: "#83869A",
+    pink: "#F74780",
+    white: "#FFFFFF",
+  },
+};
+
+const fontSize = {
+  XL: "56px",
+  L: "40px",
+  M: "32px",
+  S: "24px",
+};
+
+module.exports = {
+  theme: {
+    colors,
+    fontSize,
+  },
+};
